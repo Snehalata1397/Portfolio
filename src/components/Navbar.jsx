72b7bc10c250742a355e2064eb66aa09a6aefa05
Snehalata1397 <sahu.snehalata789@gmail.com>
2022-04-01 22:07:14 +0530

    another update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,30 +59,30 @@ const Navbar = () => {
         className={
           !nav
             ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex  flex-col justify-between items-center"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-12 text-1xl">
+        <li className="py-6 text-1xl">
           <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className="py-12 text-1xl">
+        <li className="py-6 text-1xl">
           <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className="py-12 text-1xl">
+        <li className="py-6 text-1xl">
           <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className="py-12 text-1xl">
+        <li className="py-6 text-1xl">
           <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li className="py-12 text-1xl">
+        <li className="py-6 text-1xl">
           {" "}
           <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
