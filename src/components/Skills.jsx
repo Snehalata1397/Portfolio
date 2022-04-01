b1a4877468b5a07238bb3bf4d32bcba16195cc83
Snehalata1397 <sahu.snehalata789@gmail.com>
2022-04-01 21:37:39 +0530

    new updates

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,20 +5,20 @@ import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
 import Node from "../assets/node.png";
-import FireBase from "../assets/firebase.png";
-import AWS from "../assets/aws.png";
+// import FireBase from "../assets/firebase.png";
+// import AWS from "../assets/aws.png";
 import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
 import Mongo from "../assets/mongo.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-full bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
-            Experience
+            Skills
           </p>
           <p className="py-4">// These are the technologies I've worked with</p>
         </div>
@@ -34,7 +34,7 @@ const Skills = () => {
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
-            <p className="my-4">TAILWIND</p>
+            <p className="my-4">TAILWIND CSS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
